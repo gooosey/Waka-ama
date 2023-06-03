2a724716a32b7ed92435dc370465248521cac4c5
Goose <leafmanplays@gmail.com>
2023-06-03 21:11:20 +1200

    added class

--- handle.py
+++ handle.py
@@ -5,6 +5,7 @@ racetype = "Final"
 races = []
 
 
+
 def read_file(file_path):
     try:
         with open(file_path, 'r') as file:
@@ -30,29 +31,43 @@ def find_files(race, folder_path):
                 file_path = os.path.join(folder_path, file)
                 races.append(read_file(file_path))
             
-find_folder(year, racetype, "resources")       
+find_folder(year, racetype, "resources")      
 
 
-        
-def createlane(line):
-    entries = line.split(',')
-    lane = entries[2]
-    uID = entries[3]
-    place = entries[4]
-    name = entries[5]
-    club = entries[6]
-    timetaken = entries[7]
 
-    for race in races:
-        lines = race.split('\n')
+for race in races:
+    lines = race.split('\n')
 
     for line in lines[1:]:
-        createlane(line)
+        line = line.split(',')
+        filter_line = ','.join(filter(None, line))
+        print(filter_line)
         
+class info:
+    def __init__(self, line):
+        entries = line.split(',')
+        self.lane = entries[0]
+        self.uID = entries[1]
+        self.place = entries[2]
+        self.name = entries[3]
+        self.club = entries[4]
+        self.timetaken = entries[5]
 
+for race in races:
+    lines = race.split('\n')
 
+    for line in lines[1:]:
+        line = line.split(',')
+        filter_line = ','.join(filter(None, line))
+        print(filter_line)
 
-
+        race_info = info(filter_line)
+        print(f"Lane: {race_info.lane}")
+        print(f"uID: {race_info.uID}")
+        print(f"place: {race_info.place}")
+        print(f"name: {race_info.name}")
+        print(f"club: {race_info.club}")
+        print(f"timetaken: {race_info.timetaken}")
 
 
 # Print the contents of each file
