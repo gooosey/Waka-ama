972f983836a6ca813c83eaa857f02fd75ee0e8f7
Goose <leafmanplays@gmail.com>
2023-06-07 11:25:08 +1200

    I have made and encoding

--- handle.py
+++ handle.py
@@ -1,19 +1,24 @@
 import os
 
-year = '2018'
+year = '2017'
 racetype = "Final"
 races = []
 
 # Function to read the contents of a file
 
 def read_file(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            file_content = file.read()
-        return file_content
-    except OSError:
-        print(f"Oops! Something went wrong with file: {file_path}")
-        return None
+    encodings = ['utf-8','latin-1'] # File containing these encoding
+    
+    for encoding in encodings:
+        try:
+            with open(file_path, 'r', encoding=encoding) as file:
+                file_content = file.read()
+            return file_content
+        except UnicodeDecodeError:
+            continue
+    
+    print(f"Oops! Something went wrong with file: {file_path}")
+    return None
 
 # Function to read the contents of a file
 
@@ -39,7 +44,40 @@ def find_files(race, folder_path):
 
 # Call the find_folder function to search for files          
 
-find_folder(year, racetype, "resources")      
+find_folder(year, racetype, "resources") 
+
+# Limits the commas to one instead of many 
+
+for race in races:
+    lines = race.split('\n')
+    for line in lines[1:]:
+        line = line.split(',')
+        filter_line = ','.join(filter(None, line))
+        print(filter_line)
+
+# Class to hold information about a race entry      
+
+class Info:
+    def __init__(self, line):
+        entries = [entry.strip() for entry in line.split(',')]
+
+        if len(entries) < 10:
+            self.broken = entries
+            self.valid = False
+            return
+
+        self.lane = entries[0]
+        self.uID = entries[1]
+        self.place = entries[2]
+        self.name = entries[3]
+        self.club = entries[4]
+        self.timetaken = entries[5]
+        self.pgain = entries[6]
+        self.valid = True
+
+inlist = []
+
+
 
 # Limits the commas to one instead of many 
 
@@ -49,46 +87,14 @@ for race in races:
     for line in lines[1:]:
         line = line.split(',')
         filter_line = ','.join(filter(None, line))
-        print(filter_line)
-    
-# Class to hold information about a race entry      
+        race_info = Info(filter_line)
+        if not race_info.valid:
+            print(f'Invalid race info: {race_info.broken}')
+            continue
+        inlist.append(race_info)
 
-class Info:
-    def __init__(self, line):
-        entries = line.split(',')
-        self.lane = entries[0]
-        self.uID = entries[1]
-        self.place = entries[2]
-        self.name = entries[3]
-        self.club = entries[4]
-        self.timetaken = entries[5]
-        self.pgain = entries[6]
-        self.timetaken2 = entries[7]
-        self.pgain2 = entries[8]
-        self.pgain3 = entries[9]
+# print club and place 
 
-# Process the filtered lines and create Info objects
-
-for line in lines[1:]:
-    line = line.split(',')
-    filter_line = ','.join(filter(None, line))
-    print(filter_line)
-
-    race_info = Info(filter_line)
-    print(f"Lane: {race_info.lane}")
-    print(f"uID: {race_info.uID}")
-    print(f"place: {race_info.place}")
-    print(f"name: {race_info.name}")
-    print(f"club: {race_info.club}")
-    print(f"timetaken: {race_info.timetaken}")
-    print(f"point gain: {race_info.pgain}")
-    print(f"timetaken2: {race_info.timetaken2}")
-    print(f"point gain: {race_info.pgain2}")
-    print(f"point gain: {race_info.pgain3}")
-
-
-
-# Print the contents of each file
-# for file_content in file_contents: 
-    # print(file_content) 
+for race_info in inlist:
+    print(race_info.club, race_info.place)
 
